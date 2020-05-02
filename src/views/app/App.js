4e1511d6000ecbcbf58303e4eefb08d22cc49427
Tutti Quintella <q.tuane@gmail.com>
2020-05-03 01:17:32 +0900

    structure folders and base app page

---
+++ src/views/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateEstablishment from '../establishment/CreateEstablishment';
+import Footer from '../../components/Footer';
+
+export default function App() {
+  return (
+    <div>
+      <CreateEstablishment />
+      <Footer />
+    </div>
+  );
+}
